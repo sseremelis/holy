6e0bfb447dbb9a46b8be789053760d4da90ac4d8
Sotiris Seremelis <sssotiris22@hotmail.com>
2016-11-19 23:40:38 +0200

    social popups

--- js/global.js
+++ js/global.js
@@ -1,3 +1,28 @@
+/**
+ * code from https://codepen.io/patrickkahl/pen/DxmfG
+ * jQuery function to prevent default anchor event and take the href * and the title to make a share popup
+ *
+ * @param  {[object]} e           [Mouse event]
+ * @param  {[integer]} intWidth   [Popup width defalut 500]
+ * @param  {[integer]} intHeight  [Popup height defalut 400]
+ * @param  {[boolean]} blnResize  [Is popup resizeabel default true]
+ */
+$.fn.customerPopup = function (e, intWidth, intHeight, blnResize) {
+
+    // Prevent default anchor event
+    e.preventDefault();
+
+    // Set values for window
+    intWidth = intWidth || '500';
+    intHeight = intHeight || '400';
+    strResize = (blnResize ? 'yes' : 'no');
+
+    // Set title and open popup with focus on it
+    var strTitle = ((typeof this.attr('title') !== 'undefined') ? this.attr('title') : 'Social Share'),
+        strParam = 'width=' + intWidth + ',height=' + intHeight + ',resizable=' + strResize,
+        objWindow = window.open(this.attr('href'), strTitle, strParam).focus();
+};
+
 $(document).ready(function(){
     $('body').on("animationend MSAnimationEnd webkitAnimationEnd oAnimationEnd", function(){
        console.log('end');
@@ -25,4 +50,9 @@ $(document).ready(function(){
     $('.sidebar').on('click',function(event){
         event.stopPropagation();
     });
+
+    $('.social-icons > a').on("click", function(e) {
+        $(this).customerPopup(e);
+    });
+
 });
